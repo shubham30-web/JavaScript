ea8ffb0fdc82eb3a834bb39f0c0a1f23bddd17d0
shubham kumar <shubhamkumar7328@gmail.com>
2024-09-30 22:29:31 +0530

    done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,16 +1,22 @@
-const accountId=5742101002248
-let accountEmail="shubhamkumar7328@gmail.com"
-var accountPassword = "12345"
-accountCity ="jaipur"
-let accountState=
-//accountId=2 //not allowed
-console.log(accountId);
+const accountId = 5742101002248;
+let accountEmail = "shubhamkumar7328@gmail.com";
+var accountPassword = "12345";
+accountCity = "jaipur";
+let accountState =
+  //accountId=2 //not allowed
+  console.log(accountId);
 
-accountEmail="shubh@gmail.com"
-accountCity="muzaffarpur"
-accountPassword="1@Shubham"
+accountEmail = "shubh@gmail.com";
+accountCity = "shubham";
+accountPassword = "1@Shubham";
 //console.log(accountPassword);
 //console.log(accountEmail);
 //console.log(accountCity);
-console.table([accountId, accountEmail, accountPassword,accountCity, accountState])
-alert("bhag")
+console.table([
+  accountId,
+  accountEmail,
+  accountPassword,
+  accountCity,
+  accountState,
+]);
+alert("bhag");
