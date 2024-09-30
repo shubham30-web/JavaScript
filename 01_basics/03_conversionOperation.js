0068e2e43e129307e61fdc692d508b609cb003fe
shubham kumar <shubhamkumar7328@gmail.com>
2024-09-30 19:17:54 +0530

    done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -11,14 +11,50 @@ let score = "33abc";
 // when we convert"33abc" this then it gives => NAN
 // true=>1;
 //false=>0;
-let isLoggedIn = "shubham"
+let isLoggedIn = "shubham";
 let booleanIsLoggedIn = Boolean(isLoggedIn);
 console.log(Boolean(isLoggedIn));
 //1=>true;   0=> false
 //""=>false
 //"shubham"=> true
 
-let someNumber=33
-let stringNumber= String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber = 33;
+// let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//***************operations***************** */
+
+let value = 3;
+let negValue = -3;
+//console.log(negValue);
+//console.log(value);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 % 2);
+// console.log(2 / 2);
+// console.log(2 * 2);
+// console.log(2 ** 3);
+
+let str1 = "hello";
+let str2 = " shubham";
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log('1' + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 +2);
+// console.log(1+ 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
+let nnum1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2;
+console.log(num2);
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
