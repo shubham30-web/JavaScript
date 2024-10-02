c2f270ee7aa94d232cae8b9cea325bc7eba03d6d
shubham kumar <shubhamkumar7328@gmail.com>
2024-10-02 23:11:23 +0530

    commit

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,40 @@
+// primmitive datatypes
+
+// 7 types: String, Number, Boolean, null, undefined,Symbol,BigInt
+
+// Java Script is type Language
+
+const score=100
+const scoreValue=100.3
+const isLoggedIn= false
+const outssidetemp= null
+let userEmail;
+
+const id =Symbol('123')
+const anotherId= Symbol('123')
+console.log(id ===anotherId);
+
+
+
+//const bigNumber = 334568955889n
+
+// Reference Type(NOn Primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+{
+
+    let myObj={
+    name:"shubham",
+    age: 22,
+    }
+}
+
+
+const myfunction=function(){
+    console.log("Hello world");
+
+}
+console.log(typeof scoreValue);
+console.log(typeof myfunction);
